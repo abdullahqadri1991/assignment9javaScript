a5e8d9719fa66cba4a1840938b4cc7c0dafd1d4c
abdullahqadri1991 <abdullahqadri1991@gmail.com>
2023-11-22 01:15:26 +0500

    Assignment-9

---
+++ app.js
@@ -0,0 +1,159 @@
+// Question # 1
+document.write("<h1>Question # 1</h1>")
+
+var array1 = [,,,]
+document.write(array1)
+
+
+// Question # 2
+document.write("<h1>Question # 2</h1>")
+
+var array2 = ["......."]
+document.write(array2)
+
+
+
+// Question # 3
+document.write("<h1>Question # 3</h1>")
+
+var stringArray = ["abdullah", "hashim", "madni"]
+document.write(stringArray )
+
+
+
+// Question # 4
+document.write("<h1>Question # 4</h1>")
+
+var numberArray = [100, 200, 300]
+document.write(numberArray)
+
+
+
+// Question # 5
+document.write("<h1>Question # 5</h1>")
+
+var booleanArray = [true, false]
+document.write(booleanArray)
+
+
+
+// Question # 6
+document.write("<h1>Question # 6</h1>")
+
+var mixedArray = ["Java Script", 100, true]
+document.write(mixedArray)
+
+// Question # 7
+document.write("<h1>Question # 7</h1>")
+
+var qualifications = ["1)SSC" +"<br>"+ "2)HSC"+"<br>"+ "3)BCS" +"<br>"+"4)BS" +"<br>"+"5)BCOM"+"<br>"+ "6)MS" +"<br>"+"7)M. Phil" +"<br>"+"8)PhD"]
+
+document.write(qualifications)
+
+
+// Question # 8
+document.write("<h1>Question # 8</h1>")
+
+var StudentName =["abdullah" , "zeeshan" , "kashan"]
+var abdullah = 320.
+var zeeshan = 230.
+var kashan = 480.
+ var totalMarks = 500
+
+ var  percentages1 = ((abdullah / 500 ) * 100 )
+ var  percentages2 = ((zeeshan / 500 ) * 100 )
+ var  percentages3 = ((kashan / 500 ) * 100 )
+
+ document.write("score of  Abdullah is" +abdullah+"percentages"+  percentages1+"%" + "<br>")
+ document.write("score of  Zeeshan" +zeeshan+"percentages"+  percentages2+"%" + "<br>")
+ document.write("score of  Kashan is" +kashan+"percentages"+  percentages3+"%" + "<br>")
+
+// Question # 9
+document.write("<h1>Question # 9</h1>")
+
+
+
+
+
+// Question # 10
+document.write("<h1>Question # 10</h1>")
+
+ var studentScore =[ 320 , 230 , 480 , 120 , "<br>"] 
+document.write(studentScore)
+studentScore.sort() 
+document.write(studentScore)
+
+
+
+
+// Question # 11
+document.write("<h1>Question # 11</h1>")
+
+var cityList = ["Karachi" , "Lahour" , "Islambad" , "Quetta" ,"Peshawar" ,]
+document.write("City List:" + "<br>" + cityList)
+
+ var selected =cityList.slice(2,4)
+document.write( "<br>" + "Selected City List :" + "<br>" +selected)
+
+
+
+// Question # 12
+document.write("<h1>Question # 12</h1>")
+
+
+var arr = ["This" , "is",  "my",  "cat"]
+document.write("Array" +"<br>"+  arr)
+
+var text = arr.join("")
+document.write("<br>" + "String" + "<br>" +  text )
+
+
+
+// Question # 13
+document.write("<h1>Question # 13</h1>")
+
+var devices = [`Keyboard` , `Mouse` , `Printer` , `Monitor`]
+
+
+document.write("devices" +"<br>"+  devices  + "<br>"+"<br>")
+
+
+
+
+document.write("Out:" +"<br>"+ devices[0] + "<br>")
+document.write("Out:" +"<br>"+devices[1] + "<br>")
+document.write("Out:" +"<br>"+devices[2] + "<br>")
+document.write("Out:" +"<br>"+devices[3] + "<br>")
+
+
+
+
+
+// Question # 14
+document.write("<h1>Question # 14</h1>")
+
+var devices = [`Keyboard` , `Mouse` , `Printer` , `Monitor`]
+
+
+document.write("devices" +"<br>"+  devices  + "<br>"+"<br>")
+
+
+
+
+document.write("Out:" +"<br>"+ devices[3] + "<br>")
+document.write("Out:" +"<br>"+devices[2] + "<br>")
+document.write("Out:" +"<br>"+devices[1] + "<br>")
+document.write("Out:" +"<br>"+devices[0] + "<br>")
+
+
+// Question # 15
+document.write("<h1>Question # 15</h1>")
+
+var phone = ["Apple","Samsung", "Motorola", "Nokia", "Sony", "Haier"]
+
+document.write("(1)" + " " + phone[0] + "<br>")
+document.write("(2)" + " " +  phone[1] + "<br>")
+document.write("(3)" + " " +  phone[2] + "<br>")
+document.write("(4)" + " " +  phone[3] + "<br>")
+document.write("(5)" + " " +  phone[4] + "<br>")
+document.write("(6)" + " " +  phone[5] + "<br>")
